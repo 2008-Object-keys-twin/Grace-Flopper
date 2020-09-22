2b002eb3fff1306ce87b4cdce4a761ea6735d5ed
BigMikeM <65140398+BigMikeM@users.noreply.github.com>
2020-09-22 17:56:27 -0400

    react: extended "Class" component

--- client/app.js
+++ client/app.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import {Navbar} from './components'
 import Routes from './routes'
+import Cart from './components/cart'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Cart />
       <Routes />
     </div>
   )
--- client/components/cart.jsx
+++ client/components/cart.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import {cart} from '../store'
 import {connect} from 'react-redux'
 
-const Cart = props => {
-  console.log(props)
-  return <div>Here's the cart!</div>
+class Cart extends React.Component {
+  //constructor() {
+  //  super()
+  //  // this.state = {
+  //  //   cart: []
+  //  // }
+  //}
+
+  //componentDidMount() {
+  //  const data = this.getCart(this.props.userId)
+  //  //what if user doesn't exist?
+  //  const userCart = data.products
+  //  this.setState({
+  //    cart: userCart
+  //  })
+  //}
+  render() {
+    console.log(this.state)
+    return (
+      <div>
+        <h1>Here's the cart!</h1>
+        <p />
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => ({
