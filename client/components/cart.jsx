943c9439dc27240de708d1841e91e9ba3ff28191
BigMikeM <65140398+BigMikeM@users.noreply.github.com>
2020-09-23 11:32:09 -0400

    Switching branches

--- client/components/cart.jsx
+++ client/components/cart.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
-import {cart} from '../store'
-import {connect} from 'react-redux'
+import { cart } from '../store'
+import { connect } from 'react-redux'
 
 class Cart extends React.Component {
-  //constructor() {
-  //  super()
-  //  // this.state = {
-  //  //   cart: []
-  //  // }
-  //}
+  constructor() {
+    super()
+    this.state = {
+      cart: []
+    }
+  }
 
-  //componentDidMount() {
-  //  const data = this.getCart(this.props.userId)
-  //  //what if user doesn't exist?
-  //  const userCart = data.products
-  //  this.setState({
-  //    cart: userCart
-  //  })
-  //}
+  componentDidMount() {
+    console.log(`HERE'S THIS (LITERALLY)`,this)
+    const data = this.props.getCart(this.props.userId)
+    //what if user doesn't exist?
+    const userCart = data.products
+    console.log(`HERE'S THE CURRENT STATE OF THINGS`, state)
+    this.setState({
+      cart: userCart
+    })
+  }
+
   render() {
-    console.log(this.state)
     return (
       <div>
         <h1>Here's the cart!</h1>
-        <p />
+        <ul>
+          {this.state.cart.map(item => <li key={item.id}>{item.name}</li>)}
+        </ul>
       </div>
     )
   }
