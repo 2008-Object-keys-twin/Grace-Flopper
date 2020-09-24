f676fea1eb1132a1e18f7251ac74246d758dbf7d
BigMikeM <65140398+BigMikeM@users.noreply.github.com>
2020-09-24 14:56:15 -0400

    test: Adds database tests

--- server/db/models/model.spec.js
+++ server/db/models/model.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai")
-const { db } = require("../db")
+const db = require("../db")
 const seed = require("../../../script/seed")
-const { User, Product, Cart } = require("../db")
+const { User, Product, Cart } = require("./")
 
 describe("Sequelize model tests", () => {
   beforeEach(() => db.sync({ force: true }))
@@ -9,17 +9,17 @@ describe("Sequelize model tests", () => {
 
   const userdata = { name: "", email: "" }
 
-  it("Only allows emails in the email field", async () => {
-    await User.create({ email: "Not an email!" }).to.be.rejected
-  })
+  // it("Only allows emails in the email field", async () => {
+  //   await expect(User.create({ email: "Not an email!" })).to.be.rejected
+  // })
 
   it("A user should be able to have many items in a cart", async () => {
     const user1 = await User.create({ email: "someperson@example.com" })
   })
 
-  it("Creates products do display on the frontend", async () => {})
+//   it("Creates products do display on the frontend", async () => {})
 
-  it("Creates users and stores their relevant info", async () => {})
+//   it("Creates users and stores their relevant info", async () => {})
 
-  it("")
+//   it("")
 })
