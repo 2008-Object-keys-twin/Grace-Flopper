7d339b4f012b11b7639be622f1a170c78f363bb4
Amadi Someswaran <amadi@someswaran.com>
2020-09-24 14:35:51 -0400

    test commit

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,8 +1,30 @@
-const router = require('express').Router()
-const {User, Product} = require('../db/models')
+const router = require("express").Router()
+const { User, Product } = require("../db/models")
 module.exports = router
 
-router.get('/:userId', async (req, res, next) => {
+router.put("/", async (req, res, next) => {
+  try {
+    const cart = await Cart.findAll({
+      where: {
+        userId: req.body.userId,
+        productId: req.body.productId
+      }
+    })
+    if (!cart) {
+      await Cart.create({
+        userId: req.body.userId,
+        productId: req.body.productId,
+        quantity: 1
+      })
+    } else {
+      cart.quantity++
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get("/:userId", async (req, res, next) => {
   try {
     const cart = await User.findAll({
       where: {
@@ -13,7 +35,7 @@ router.get('/:userId', async (req, res, next) => {
           model: Product
         }
       ],
-      attributes: ['id']
+      attributes: ["id"]
     })
     res.json(cart)
   } catch (err) {
