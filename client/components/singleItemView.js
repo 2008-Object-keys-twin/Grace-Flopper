d4e2872daca90dad9a5ca60adef23feae1e391cb
Chikara Takahashi <chtakahashi10@gmail.com>
2020-09-24 17:24:30 -0400

    feat: adds more information to singleItemView. refactor: removed attribute limitations on products route

--- client/components/singleItemView.js
+++ client/components/singleItemView.js
@@ -6,10 +6,19 @@ export class SingleItemView extends React.Component {
     const [thisItem] = this.props.items.filter(
       (item) => item.id === +this.props.match.params.productId
     )
+    console.log(thisItem)
     return (
       <div>
         <h3>Here is the item you were looking for: </h3>
-        {!thisItem ? <p>Loading...</p> : <p>{thisItem.name}</p>}
+        {!thisItem ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <p>{thisItem.name}</p>
+            <p>{thisItem.description}</p>
+            <img src={thisItem.imageUrl} />
+          </>
+        )}
       </div>
     )
   }
