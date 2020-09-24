54ccad12c94b7d5a7ed4b1156977fdbb8ac41e58
Chikara Takahashi <chtakahashi10@gmail.com>
2020-09-24 11:58:31 -0400

    refactor: database schema is now set in a way that does not require array indexing to be correct. order and orderDetails added

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,26 +1,6 @@
 const Sequelize = require("sequelize")
 const db = require("../db")
 
-const Order = db.define("order", {
-  userId: {
-    type: Sequelize.INTEGER
-  },
-  itemIds: {
-    type: Sequelize.ARRAY(Sequelize.INTEGER)
-  },
-  prices: {
-    type: Sequelize.ARRAY(Sequelize.FLOAT)
-  },
-  orderQuantities: {
-    type: Sequelize.ARRAY(Sequelize.INTEGER)
-  }
-})
-
-Order.prototype.calculateTotal = function() {
-  return this.prices.reduce(function(accumulator, currentValue, index) {
-    return (accumulator +=
-      currentValue * this.orderQuantities[index])
-  }, 0)
-}
+const Order = db.define("order", {})
 
 module.exports = Order
---
+++ server/db/models/orderDetail.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize")
+const db = require("../db")
+
+const OrderDetail = db.define("orderDetail", {
+  itemId: {
+    type: Sequelize.INTEGER
+  },
+  price: {
+    type: Sequelize.FLOAT
+  },
+  orderQuantity: {
+    type: Sequelize.INTEGER
+  }
+}
+)
+
+module.exports = OrderDetail
