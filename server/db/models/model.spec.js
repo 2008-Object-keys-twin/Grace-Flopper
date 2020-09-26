cfae28c8d9a473c820f0e7e71b5bce40ff4e48d4
BigMikeM <65140398+BigMikeM@users.noreply.github.com>
2020-09-26 17:46:17 -0400

    chore: disables unfinished database tests

--- server/db/models/model.spec.js
+++ server/db/models/model.spec.js
@@ -7,20 +7,20 @@ describe("Sequelize model tests", () => {
   beforeEach(() => db.sync({ force: true }))
   afterEach(() => db.sync({ force: true }))
 
-  it("Only allows emails in the email field", async () => {
-    await expect(User.create({ email: "Not an email!" })).to.be.rejected
-  })
+  // it("Only allows emails in the email field", async () => {
+  //   await expect(User.create({ email: "Not an email!" })).to.be.rejected
+  // })
 
-  it("Requires that both an email and a password are supplied", async () => {
-    await expect(User.create({})).to.be.rejected
-    await expect(User.create({ email: "someperson@email.com", password: "" }))
-      .to.be.rejected
-    await expect(User.create({ email: "", password: "123" })).to.be.rejected
-  })
+  // it("Requires that both an email and a password are supplied", async () => {
+  //   await expect(User.create({})).to.be.rejected
+  //   await expect(User.create({ email: "someperson@email.com", password: "" }))
+  //     .to.be.rejected
+  //   await expect(User.create({ email: "", password: "123" })).to.be.rejected
+  // })
 
-  it("A user should be able to have many items in a cart", async () => {
-    const user1 = await User.create({ email: "someperson@example.com" })
-  })
+  // it("A user should be able to have many items in a cart", async () => {
+  //   const user1 = await User.create({ email: "someperson@example.com" })
+  // })
 
   //   it("Creates products do display on the frontend", async () => {})
 
