ecc897eb37f79f6d9b75bd13d9a246131be28468
BigMikeM <65140398+BigMikeM@users.noreply.github.com>
2020-09-27 17:59:21 -0400

    feat: adds working "Add Products" form and button in adminPage

--- client/components/adminPage.js
+++ client/components/adminPage.js
@@ -1,10 +1,33 @@
 import React from "react"
 import { connect } from "react-redux"
-import { fetchAllUsers } from "../store"
+import { fetchAllUsers, addNewProduct } from "../store"
 
 class AdminPage extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      name: "",
+      color: "",
+      price: 0
+    }
+  }
+
   componentDidMount() {
     this.props.fetchUsers(this.props.administratorCheck)
+  }
+
+  onChange(event) {
+    const name = event.target.name
+    const value = event.target.value
+    const newProduct = {
+      [name]: value
+    }
+    this.setState(newProduct)
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props.addProduct(this.state)
   }
 
   render() {
@@ -22,6 +45,28 @@ class AdminPage extends React.Component {
               </div>
             )
           })}
+        <hr />
+        <h2>Add new products to the database</h2>
+        <form
+          id="add-product-form"
+          onSubmit={(event) => this.handleSubmit(event)}
+        >
+          <label onChange={() => this.onChange(event)}>
+            <span>Product name:</span>
+            <input name="name" type="text" />
+          </label>
+          <br />
+          <label onChange={() => this.onChange(event)}>
+            <span>Color:</span>
+            <input name="color" type="text" />
+          </label>
+          <br />
+          <label onChange={() => this.onChange(event)}>
+            <span>Price:</span>
+            <input name="price" type="text" />
+          </label>
+          <button type="submit">Add Product</button>
+        </form>
       </div>
     )
   }
@@ -35,6 +80,9 @@ const mapDispatchToProps = (dispatch) => {
   return {
     fetchUsers: (bool) => {
       dispatch(fetchAllUsers(bool))
+    },
+    addProduct: (product) => {
+      dispatch(addNewProduct(product))
     }
   }
 }
