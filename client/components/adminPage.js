1630f1a7b34d50c7be9c3cf06dc53b68b9278011
BigMikeM <65140398+BigMikeM@users.noreply.github.com>
2020-09-29 08:12:53 -0400

    feat: changes admin product add form to bootstrap

--- client/components/adminPage.js
+++ client/components/adminPage.js
@@ -2,6 +2,7 @@ import React from "react"
 import { connect } from "react-redux"
 import { fetchAllUsers, addNewProduct } from "../store"
 import Button from "react-bootstrap/Button"
+import Form from "react-bootstrap/Form"
 
 class AdminPage extends React.Component {
   constructor() {
@@ -36,6 +37,7 @@ class AdminPage extends React.Component {
   }
 
   render() {
+    console.log(this.state)
     const { data } = this.props.allUsers || []
     return (
       <div id="admin-page-body">
@@ -56,46 +58,52 @@ class AdminPage extends React.Component {
         </div>
         <hr />
         <h2>Add new products to the database</h2>
-        <form
+        <Form
           id="add-product-form"
           onSubmit={(event) => this.handleSubmit(event)}
         >
-          <label onChange={() => this.onChange(event)}>
-            <span>Product name:</span>
-            <input name="name" type="text" />
-          </label>
-          <br />
-          <label onChange={() => this.onChange(event)}>
-            <span>Color:</span>
-            <input name="color" type="text" />
-          </label>
-          <br />
-          <label onChange={() => this.onChange(event)}>
-            <span>Price:</span>
-            <input name="price" type="text" />
-          </label>
-          <br />
-          <label onChange={() => this.onChange(event)}>
-            <span>Image URL:</span>
-            <input name="imageUrl" type="text" />
-          </label>
-          <br />
-          <label onChange={() => this.onChange(event)}>
-            <span>Size (S, M, L):</span>
-            <input name="size" type="text" />
-          </label>
-          <br />
-          <label onChange={() => this.onChange(event)}>
-            <span>Filters (Men, Women, or Children):</span>
-            <input name="filter" type="text" />
-          </label>
-          <br />
-          <label onChange={() => this.onChange(event)}>
-            <span>Quantity:</span>
-            <input name="quantity" type="text" />
-          </label>
+          <Form.Group onChange={() => this.onChange(event)}>
+            <Form.Label>Product name:</Form.Label>
+            <Form.Control name="name" type="text" placeholder="Product name" />
+          </Form.Group>
+          <Form.Group onChange={() => this.onChange(event)}>
+            <Form.Label>Color:</Form.Label>
+            <Form.Control name="color" type="text" placeholder="Color" />
+          </Form.Group>
+          <Form.Group onChange={() => this.onChange(event)}>
+            <Form.Label>Price:</Form.Label>
+            <Form.Control name="price" type="text" placeholder="$$$" />
+          </Form.Group>
+          <Form.Group onChange={() => this.onChange(event)}>
+            <Form.Label>Image URL:</Form.Label>
+            <Form.Control
+              name="imageUrl"
+              type="text"
+              placeholder="http://example.com"
+            />
+          </Form.Group>
+          <Form.Group onChange={() => this.onChange(event)}>
+            <Form.Label>Size:</Form.Label>
+            <Form.Control as="select" name="size" type="text">
+              <option>S</option>
+              <option>M</option>
+              <option>L</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group onChange={() => this.onChange(event)}>
+            <Form.Label>Filters:</Form.Label>
+            <Form.Control as="select" name="filter" type="text">
+              <option>women</option>
+              <option>men</option>
+              <option>children</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group onChange={() => this.onChange(event)}>
+            <Form.Label>Quantity:</Form.Label>
+            <Form.Control name="quantity" type="text" />
+          </Form.Group>
           <Button type="submit">Add Product</Button>
-        </form>
+        </Form>
       </div>
     )
   }
